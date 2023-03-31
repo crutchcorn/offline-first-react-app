152bd7a133d5e4e851823619dafcf30d1e166f70
Corbin Crutchley <git@crutchcorn.dev>
2023-03-30 22:16:25 -0700

    initial work at loading data in client

--- src/client-pages/index.tsx
+++ src/client-pages/index.tsx
@@ -1,3 +1,22 @@
+import { useQuery } from "@tanstack/react-query";
+import { getPeopleList } from "@/services/people";
+
 export const HomeClient = () => {
-  return null;
+  const { data: people, isLoading } = useQuery(["people"], {
+    queryFn: async () => {
+      return await getPeopleList();
+    },
+  });
+
+  if (isLoading) return <div>Loading...</div>;
+
+  return (
+    <div>
+      <ul>
+        {people!.map((person) => (
+          <li key={person.id}>{person.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
 };
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
-import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const client = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <QueryClientProvider client={client}>
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  );
 }

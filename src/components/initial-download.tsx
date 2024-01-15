c40c344973ac960dfc3a3f84168ef031ebe6e75c
Corbin Crutchley <git@crutchcorn.dev>
2024-01-15 12:16:09 -0800

    fix: initial download no longer results in crash at end

--- src/components/initial-download.tsx
+++ src/components/initial-download.tsx
@@ -1,10 +1,13 @@
 import {
 	type Dispatch,
+	forwardRef,
 	memo,
 	type SetStateAction,
 	useCallback,
+	useImperativeHandle,
 	useLayoutEffect,
 	useMemo,
+	useRef,
 	useState,
 } from "react";
 import {
@@ -30,75 +33,82 @@ const setStoredInitialLoadedMeta = (val: InitialLoadedMetaStatus) => {
 	localStorage.setItem(INITIAL_LOADED_META_KEY, val);
 };
 
-type QueryMeta = {
-	isSuccess: boolean;
-	isError: boolean;
-	refetch: UseQueryResult<boolean, unknown>["refetch"];
-};
+type Refetch = UseQueryResult<boolean, unknown>["refetch"];
 
 interface DownloadInitialDataProps {
 	initialLoadedMeta: InitialLoadedMetaStatus;
+	setReactiveInitialLoadedMeta: (val: InitialLoadedMetaStatus) => void;
 	setChunkMeta: Dispatch<SetStateAction<{ current: number; total: number }>>;
-	setQueryMeta: (val: QueryMeta) => void;
 }
 
-const DownloadInitialDataBase = ({
-	initialLoadedMeta,
-	setChunkMeta,
-	setQueryMeta,
-}: DownloadInitialDataProps) => {
-	const queryClient = useQueryClient();
+const DownloadInitialDataBase = forwardRef<Refetch, DownloadInitialDataProps>(
+	({ initialLoadedMeta, setChunkMeta, setReactiveInitialLoadedMeta }, ref) => {
+		const queryClient = useQueryClient();
 
-	const { isSuccess, isError, refetch } = useQuery({
-		queryKey: initialDownloadKeys.status(initialLoadedMeta),
-		queryFn: async ({ signal }) => {
-			// Set the stored value so that we can warn if the user closes the app before the download is finished.
-			setStoredInitialLoadedMeta("IN_PROGRESS");
-			// Clear the query cache so that we can know that the app doesn't have any stale data
-			queryClient.removeQueries({ queryKey: customerKeys.all });
-			// Get the full database to store in the cache
-			const database = await getPeopleDatabaseList({ signal });
-			const CHUNK_SIZE = 100;
-			const totalChunks = Math.ceil(database.length / CHUNK_SIZE);
-			setChunkMeta({
-				current: 0,
-				total: totalChunks,
-			});
+		const { isSuccess, isError, refetch } = useQuery({
+			queryKey: initialDownloadKeys.status(initialLoadedMeta),
+			queryFn: async ({ signal }) => {
+				// Set the stored value so that we can warn if the user closes the app before the download is finished.
+				setStoredInitialLoadedMeta("IN_PROGRESS");
+				// Clear the query cache so that we can know that the app doesn't have any stale data
+				queryClient.removeQueries({ queryKey: customerKeys.all });
+				// Get the full database to store in the cache
+				const database = await getPeopleDatabaseList({ signal });
+				const CHUNK_SIZE = 100;
+				const totalChunks = Math.ceil(database.length / CHUNK_SIZE);
+				setChunkMeta({
+					current: 0,
+					total: totalChunks,
+				});
 
-			const pickedData: StoredCustomerList = [];
-			// For each customer detail, take and chunk (so it doesn't block the main thread on 15,000 requests)
-			// Then, store the details in the query cache so we can load it offline
-			await chunkForEach({
-				arr: database,
-				eachChunkFn() {
-					setChunkMeta((prev) => ({
-						...prev,
-						current: prev.current + 1,
-					}));
-				},
-				eachItemfn(customer) {
-					queryClient.setQueryData(customerKeys.detail(customer.id), customer);
-					const pickedCustomer = convertPersonDetailsToPersonList(customer);
-					pickedData.push(pickedCustomer as never);
-				},
-				delayTime: () => 100,
-				signal,
-				chunkSize: CHUNK_SIZE,
-			});
-			if (signal.aborted) return false;
-			queryClient.setQueryData(customerKeys.lists(), pickedData);
-			return true;
-		},
-		retry: 0,
-		enabled: initialLoadedMeta === "UNFINISHED",
-	});
+				const pickedData: StoredCustomerList = [];
+				// For each customer detail, take and chunk (so it doesn't block the main thread on 15,000 requests)
+				// Then, store the details in the query cache so we can load it offline
+				await chunkForEach({
+					arr: database,
+					eachChunkFn() {
+						setChunkMeta((prev) => ({
+							...prev,
+							current: prev.current + 1,
+						}));
+					},
+					eachItemfn(customer) {
+						queryClient.setQueryData(
+							customerKeys.detail(customer.id),
+							customer,
+						);
+						const pickedCustomer = convertPersonDetailsToPersonList(customer);
+						pickedData.push(pickedCustomer as never);
+					},
+					delayTime: () => 100,
+					signal,
+					chunkSize: CHUNK_SIZE,
+				});
+				if (signal.aborted) return false;
+				queryClient.setQueryData(customerKeys.lists(), pickedData);
+				return true;
+			},
+			retry: 0,
+			enabled: initialLoadedMeta === "UNFINISHED",
+		});
 
-	useLayoutEffect(() => {
-		setQueryMeta({ isSuccess, isError, refetch });
-	}, [isSuccess, isError, refetch]);
+		// Do not try to merge this with the useImperativeHandle below, it will cause an infinite loop
+		// We don't need the refetch to be reactive anyway
+		useLayoutEffect(() => {
+			if (isSuccess) {
+				setReactiveInitialLoadedMeta("FINISHED");
+				setStoredInitialLoadedMeta("FINISHED");
+			} else if (isError) {
+				setReactiveInitialLoadedMeta("ERRORED");
+				setStoredInitialLoadedMeta("ERRORED");
+			}
+		}, [isSuccess, isError]);
 
-	return null;
-};
+		useImperativeHandle(ref, () => refetch, [refetch]);
+
+		return null;
+	},
+);
 
 // Use `memo` to avoid re-renders (and therefore refetching)
 const DownloadInitialData = memo(DownloadInitialDataBase);
@@ -106,7 +116,7 @@ const DownloadInitialData = memo(DownloadInitialDataBase);
 DownloadInitialData.displayName = "DownloadInitialData";
 
 export const useInitialDownload = () => {
-	const [_initialLoadedMeta, setReactiveInitialLoadedMeta] = useState(
+	const [initialLoadedMeta, setReactiveInitialLoadedMeta] = useState(
 		(): InitialLoadedMetaStatus => {
 			const rawMeta = localStorage.getItem(INITIAL_LOADED_META_KEY);
 			if (!rawMeta) return "UNFINISHED";
@@ -120,30 +130,17 @@ export const useInitialDownload = () => {
 		total: 0,
 	});
 
-	const [downloadQueryMeta, setDownloadQueryMeta] = useState<QueryMeta | null>(
-		null,
-	);
-
-	const initialLoadedMeta = useMemo(() => {
-		if (downloadQueryMeta?.isSuccess) {
-			setStoredInitialLoadedMeta("FINISHED");
-			return "FINISHED";
-		}
-		if (downloadQueryMeta?.isError) {
-			setStoredInitialLoadedMeta("ERRORED");
-			return "ERRORED";
-		}
-		return _initialLoadedMeta;
-	}, [_initialLoadedMeta, downloadQueryMeta]);
+	const refetchRef = useRef<Refetch>(null);
 
 	const DownloadComponent = useCallback(() => {
 		// The passed properties **MUST** be referentially stable, except for initialLoadedMeta
 		// otherwise, it will attempt to re-query every time one of these properties changes
 		return (
 			<DownloadInitialData
-				setQueryMeta={setDownloadQueryMeta}
 				setChunkMeta={setChunkMeta}
 				initialLoadedMeta={initialLoadedMeta}
+				setReactiveInitialLoadedMeta={setReactiveInitialLoadedMeta}
+				ref={refetchRef}
 			/>
 		);
 	}, [initialLoadedMeta]);
@@ -163,8 +160,8 @@ export const useInitialDownload = () => {
 		const retry = useCallback(() => {
 			setStoredInitialLoadedMeta("UNFINISHED");
 			setReactiveInitialLoadedMeta("UNFINISHED");
-			void downloadQueryMeta?.refetch();
-		}, [downloadQueryMeta]);
+			void refetchRef.current?.();
+		}, []);
 
 		if (initialLoadedMeta === "IN_PROGRESS") {
 			return (
